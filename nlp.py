4f33d848835aacba563e7a3d047813def80639f6
Eric Ngo <quocdat32461997@yahoo.com>
2021-05-10 12:07:27 -0500

    NLP: added the ACQUIRE filling function

--- nlp.py
+++ nlp.py
@@ -40,31 +40,24 @@ class NLP(object):
         hypernyms, hyponyms, meronyms, holonyms = [], [], [], []
         for i in tqdm(range(len(input)), dynamic_ncols=True):
             sent = input[i]
+            doc = self._nlp(sent)
+
             # add placeholder for each sentence
             pos.append([])
             tag.append([])
             dep.append([])
-            ents.append([(ent.text, ent.label_) for ent in self._nlp(sent).ents])
-            lem.append({'dep': None, 'head_text': None, 'head_pos': None, 'children': []})
+            lem.append([])
+            ents.append([(ent.text, ent.label_) for ent in doc.ents])
             hypernyms.append([])
             hyponyms.append([])
             meronyms.append([])
             holonyms.append([])
-            
-            # get doc with features
-            doc = self._nlp(sent)
-            
-            # parse features
+
             for tok in doc:
+                lem[-1].append(tok.lemma_)
                 pos[-1].append(tok.pos_) # pos
                 tag[-1].append(tok.tag_) # tag
                 dep[-1].append(tok.dep_) # dep
-
-                # lemma
-                lem[-1]['dep'] = tok.dep_
-                lem[-1]['head_text'] = tok.head.text
-                lem[-1]['head_pos'] = tok.head.pos_
-                lem[-1]['children'] = list(tok.children)
 
                 # wordnet features
                 hypernyms[-1].append(
@@ -75,7 +68,6 @@ class NLP(object):
                         [x.part_meronyms() for x in wordnet.synsets(tok.text)])
                 holonyms[-1].append(
                         [x.part_holonyms() for x in wordnet.synsets(tok.text)])
-
         return {'lem' : lem,
                 'pos' : pos, 
                 'tag' : tag, 
@@ -91,12 +83,54 @@ class NLP(object):
     
     def fill_acquire(self, sents, features):
         res = []
-        for x in features['ents']:
-            print(x)
-            
-        return None
+        for i, lemmas, ents, dep in zip(range(len(sents)), features['lem'], features['ents'], features['dep']):
+
+            # in sentence
+            try:
+                # find index of acquire in lemmas
+                index = lemmas.index('acquire') or lemmas.index('buy')
+
+                # parse entities
+                def _parse_ents(inputs):
+                    """
+                    Parse entities into ORGs and DATEs
+                    """
+                    dates, orgs = [], []
+                    for ent in inputs:
+                        if ent[-1] == 'DATE':
+                            dates.append(ent[0])
+                        elif ent[-1] == 'ORG':
+                            orgs.append(ent[0])
+                    return dates, orgs
+
+                dates, orgs = _parse_ents(ents)
+
+                # get all possible ACQUIRE templates
+                if len(dates) < 1 or len(orgs) < 2:
+                    # find no entites
+                    continue
+
+                # get buyer
+                if lemmas[dep.index('nsubj')] == orgs[0]:
+                    # active
+                    buyer = orgs.pop(0)
+                elif lemmas[dep.index('nsubjpass')] == orgs[-1]:
+                    # passive
+                    buyer = orgs.pop()
+                else:
+                    buyer = 'None'
+                
+                # get sellers
+                if len(dates) == 1:
+                    res.append((buyer, orgs.pop(0), dates[-1]))
+                else:
+                    while dates and orgs:
+                        res.append((buyer, orgs.pop(0), dates.pop(0)))
+            except:
+                pass
+        return res
     
-    def fill_part_of(self):
+    def fill_part_of(self, sents, features):
         return None
 
     def fill(self, sents, features):
@@ -117,8 +151,10 @@ class NLP(object):
             
         # ACQUIRE template
         templates['ACQUIRE'] = self.fill_acquire(sents, features)
+        print(templates['ACQUIRE'])
             
         # PART-OF template
+        templates['PART_OF'] = self.fill_part_of(sents, features)
             
         return templates
 
@@ -138,14 +174,10 @@ class NLP(object):
         # to sentences
         sents = [sent.text for sent in input_doc.sents]
 
-        # get corefs
-        corefs = input_doc._.coref_clusters if input_doc._.has_coref else []
-
         # to tokens
         tokens = [token.text for token in input_doc]
 
         # get pos, tags, lemmas, and dependency
         features = self._get_features(sents)
-        features['corefs'] = corefs
 
         return sents, tokens, features
